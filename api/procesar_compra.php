b98278af44a64f9fd9be45d79464e35347d92374
Evelyn Lara <evelynlara806@gmail.com>
2025-12-05 22:24:39 -0600

    compra

---
+++ api/procesar_compra.php
@@ -0,0 +1,29 @@
+<?php
+header('Content-Type: application/json');
+
+$conn = pg_connect("host=localhost dbname=tienda_coppel_escolar user=postgres password=TU_PASSWORD");
+
+$data = json_decode(file_get_contents("php://input"), true);
+$carrito = $data['carrito'];
+
+pg_query($conn, "BEGIN");
+
+foreach($carrito as $item){
+    $sql = "INSERT INTO ventas (producto_id, cantidad, precio)
+            VALUES ($1, $2, $3)";
+
+    $res = pg_query_params($conn, $sql, [
+        $item['id'],
+        $item['qty'],
+        $item['precio']
+    ]);
+
+    if(!$res){
+        pg_query($conn, "ROLLBACK");
+        echo json_encode(["success"=>false]);
+        exit;
+    }
+}
+
+pg_query($conn, "COMMIT");
+echo json_encode(["success"=>true]);
